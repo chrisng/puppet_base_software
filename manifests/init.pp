786b548bd11cbc78e94c7b265e490773cdb96662
Chris Ng @_chrisng_ <chaoselixir@gmail.com>
2014-11-04 11:18:30 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class base_software {
 
-  $base_software = ['htop', 'iotop', 'glances', 'curl', 'vim' ]
+  $base_software = ['htop', 'iotop', 'glances', 'curl', 'vim', 'ethtool' ]
   package { "$base_software": ensure => latest, }
 
 
